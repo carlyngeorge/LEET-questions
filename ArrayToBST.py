93fd5ef2379785645a84a2f7f140fe69aa58936e
carlyngeorge <carlyngeorge06@gmail.com>
2026-01-05 11:40:40 +0400

    Create ArrayToBST.py

---
+++ ArrayToBST.py
@@ -0,0 +1,8 @@
+def sortedArrayToBST(nums):
+    if not nums:
+        return None
+    mid = len(nums)//2
+    root = TreeNode(nums[mid])
+    root.left = sortedArrayToBST(nums[:mid])
+    root.right = sortedArrayToBST(nums[mid+1:])
+    return root
