036e17c62c855f0a2c01740d227a3996c77935be
carlyngeorge <carlyngeorge06@gmail.com>
2026-01-05 11:38:26 +0400

    Create Productexceptself.py

---
+++ Productexceptself.py
@@ -0,0 +1,11 @@
+def productExceptSelf(nums):
+    res=[1]*len(nums)
+    p=1
+    for i in range(len(nums)):
+        res[i]=p
+        p*=nums[i]
+    p=1
+    for i in range(len(nums)-1,-1,-1):
+        res[i]*=p
+        p*=nums[i]
+    return res
