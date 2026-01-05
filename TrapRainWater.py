e4f41aea35e95471e9b33d2d2820229ef6b8742c
carlyngeorge <carlyngeorge06@gmail.com>
2026-01-05 11:49:23 +0400

    Create TrapRainWater.py

---
+++ TrapRainWater.py
@@ -0,0 +1,14 @@
+def trap(height):
+    l,r=0,len(height)-1
+    lm=rm=0
+    res=0
+    while l<r:
+        if height[l]<height[r]:
+            lm=max(lm,height[l])
+            res+=lm-height[l]
+            l+=1
+        else:
+            rm=max(rm,height[r])
+            res+=rm-height[r]
+            r-=1
+    return res
